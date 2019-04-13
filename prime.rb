fdd4b666a90cf8c0ea3e0f08456a9b2223058671
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-13 07:29:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,7 @@
 # Add  code here!
 
 
-prime = []
-
-prime.each do |x|
-  x / x = 1
-  prime += 1
+def prime?(integer)
+  (2..integer - 1).each {|x| return false if (integer % x) == 0 }
+  true
 end
